0bf96db2a27ab373832ca2e4634905dc1a5a5a40
asharinovaldy <asharinovaldi6319@gmail.com>
2023-11-26 00:50:07 +0800

    - [FE] Page Homepage - [FE] Component CarouselBanner - [FE] Component CardEvent - [FE] Component CardBlog

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,101 @@
+import CardBlog from "@/components/CardBlog";
+import CardEvent from "@/components/CardEvent";
+import CarouselBanner from "@/components/CarouselBanner";
 import Header from "@/components/Header";
 import Layout from "@/components/Layout";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import Slider from "react-slick";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: false,
+    centerPadding: "8px",
+    slidesToShow: 1.1,
+    speed: 500,
+    arrows: false,
+    dots: true,
+  };
   return (
     <>
       <Layout>
         <Header type="landing" />
+        <CarouselBanner />
+        <div className="container mx-auto px-4 flex flex-col gap-5 mt-[40px]">
+          <div className="flex gap-1">
+            <div className="text-black flex flex-col justify-center items-center text-center gap-1">
+              <Image
+                src="/images/empowered.png"
+                width={52}
+                height={52}
+                alt="Empowered"
+              />
+              <p className="text-xs">Empowered</p>
+              <p className="text-xs font-light text-gray-400">
+                Pemberdayaan UMKM
+              </p>
+            </div>
+            <div className="text-black flex flex-col justify-center items-center text-center gap-1">
+              <Image
+                src="/images/entepreneur.png"
+                width={52}
+                height={52}
+                alt="Entepreneur"
+              />
+              <p className="text-xs">Entepreneur</p>
+              <p className="text-xs font-light text-gray-400">Pameran UMKM</p>
+            </div>
+            <div className="text-black flex flex-col justify-center items-center text-center gap-1">
+              <Image
+                src="/images/education.png"
+                width={52}
+                height={52}
+                alt="Education"
+              />
+              <p className="text-xs">Education</p>
+              <p className="text-xs font-light text-gray-400">
+                Berbagi Pelatihan
+              </p>
+            </div>
+            <div className="text-black flex flex-col justify-center items-center text-center gap-1">
+              <Image
+                src="/images/festival.png"
+                width={52}
+                height={52}
+                alt="Festival"
+              />
+              <p className="text-xs">Festival</p>
+              <p className="text-xs font-light text-gray-400">
+                Games dan Hiburan
+              </p>
+            </div>
+          </div>
+          <CardEvent
+            title="Pelatihan UMKM Fest 2023"
+            subTitle="Daftar Sekarang, Jangan Sampai Kehabisan!"
+            image="/images/pelatihan-graphic 1 1.png"
+            btnLabel="Daftar Pelatihan"
+            buttonVariant="secondary"
+          />
+          <CardEvent
+            title=""
+            subTitle="Tiket UMKMFest 2023"
+            image="/images/Ticket 1.png"
+            btnLabel="Tiket Masuk"
+            buttonVariant="primary"
+          />
+          <div>
+            <p className="font-semibold text-xl text-black">Info Terbaru</p>
+            <Slider {...settings}>
+              <CardBlog />
+              <CardBlog />
+            </Slider>
+          </div>
+        </div>
       </Layout>
       {/* <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
