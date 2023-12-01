cb02093344f87b36108cc285a151f054c8aa38f3
Agung Kurniawan <agung41149@gmail.com>
2023-12-01 08:45:56 +0800

    - add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { initGA, logPageView } from "@/components/Analytics";
 import "@/styles/globals.css";
 import { Outfit } from "next/font/google";
 import "slick-carousel/slick/slick.css";
@@ -6,6 +9,17 @@ import "slick-carousel/slick/slick-theme.css";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+    router.events.on('routeChangeComplete', logPageView);
+  }, [router.events]);
+
   return (
     <main className={outfit.className}>
       <Component {...pageProps} />
